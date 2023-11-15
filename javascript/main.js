37a3d35a2af84a452f2feb25a9b2948996c0d9e6
A-Hajji <302188637@student.rocmondriaan.nl>
2023-11-15 16:04:30 +0100

    hw 15-11-2023
    huiswerk

---
+++ javascript/main.js
@@ -0,0 +1,63 @@
+const person =  {
+    firstName: 'adib',
+    lastName: 'hajji',
+    age: 18,
+    course: 'ict sd',
+    score: 200,
+};
+
+
+console.log("Voornaam is: ", person.firstName);
+console.log("Achternaam is: ", person.lastName);
+console.log("leeftijd is: ", person.age);
+console.log("opleiding is: ", person.course);
+console.log("score is: ", person.score);
+
+
+const user = {
+    userName: 'cat123',
+    titel: 'cat',
+    story: 'cat in tree',
+    date: '15-11-2023',
+    views: 1200,
+};
+
+
+console.log("gebruikersnaam: ", user.userName);
+console.log("titel: ", user.titel);
+console.log("story: ", user.story);
+console.log("date: ", user.date);
+console.log("weergaven: ", user.views);
+
+const foodRecepie1 = {
+    titel: 'pie', 
+    description: 'apple pie',
+    healthy: 'very bad',
+};
+
+
+console.log("gebruikersnaam: ", foodRecepie1.titel);
+console.log("beschrijving: ", foodRecepie1.description);
+console.log("gezond voor je: ", foodRecepie1.healthy);
+
+
+const foodRecepie2 = {
+    titel: 'pizza', 
+    description: 'pizza pepperoni',
+    healthy: 'very bad',
+};
+
+console.log("gebruikersnaam: ", foodRecepie2.titel);
+console.log("beschrijving: ", foodRecepie2.description);
+console.log("gezond voor je: ", foodRecepie2.healthy);
+
+
+const foodRecepie3 = {
+    titel: 'chickenRice', 
+    description: 'chicken with rice',
+    healthy: 'very good',
+};
+
+console.log("gebruikersnaam: ", foodRecepie3.titel);
+console.log("beschrijving: ", foodRecepie3.description);
+console.log("gezond voor je: ", foodRecepie3.healthy);
